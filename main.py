b8b4c4de638cfde1a79c22532b2b9b7aa131f7b3
Abiezer Reyes <master21delakal@live.com>
2016-06-06 21:26:17 -0400

    main.py mejorado

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import os
 import jinja2
 import webapp2
-import re
+
 #conectando a la base de datos 
 from google.appengine.ext import db
 from imports import hashear
-#cambio de linea
+
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 ##
 class Handler(webapp2.RequestHandler):
-	def write(self, *a, **kw):
-		self.response.out.write(*a, **kw)
+  def write(self, *a, **kw):
+    self.response.out.write(*a, **kw)
 
-	def render_str(self, template, **params):
-		t = jinja_env.get_template(template)
-		return t.render(params)
+  def render_str(self, template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
 
-	def render(self, template, **kw):
-		self.write(self.render_str(template, **kw))
+  def render(self, template, **kw):
+    self.write(self.render_str(template, **kw))
 
 #clase database
 class Db(db.Model):
@@ -27,9 +27,9 @@ class Db(db.Model):
       nombre=db.StringProperty(required = False)
       reguser=db.StringProperty(required = True)
       regpass=db.StringProperty(required = True) 
-   	  
+      
 class MainHandler(Handler):
-	#body de html
+  #body de html
     def validar(self, user, pword):
       datos= db.GqlQuery("SELECT * FROM Db " "ORDER BY fecha DESC") 
 
@@ -38,7 +38,6 @@ class MainHandler(Handler):
     #       #
       secret=self.start_secret(user)
       ps=self.check_secure_val(ps)
-
       if secret==us and ps==pword:
         return self.render("content.html", datos=datos, user=user.title(), n=1)
       else:
@@ -80,10 +79,11 @@ class MainHandler(Handler):
       name=self.getP("nombre")
     ##              ##
       if user and pword and name:
-        #self.write(check_user(user))
+        salt=self.salts()
+        pword=self.start_check(pword, user)
         ##
         #self.write(pword)
-        self.redirect("/token=reg?username="+user+"&pword="+pword+"&name="+name+"&n=1")
+        self.redirect("/token=reg?username="+user+"&pword="+salt+"&name="+name+"&n=1")
 
       else:
         self.redirect("/token=login")
@@ -92,31 +92,34 @@ class MainHandler(Handler):
       pword=self.hash_keys(pword)
       pword=self.secret(pword)
       return pword 
-
-
-USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-def check_user(user):
-  return user 
-
-PASS_RE = re.compile(r"^.{3,20}$")
-def check_pass(pword):
-  return pword and PASS_RE.match(pword)
+    
+    def start_check(self, pword, user):
+      pword=self.start_secret(pword)
+      pword=pword+self.salts()
+      self.add_cookies("ps", pword)
+      pword=self.check_secure_val(pword)
+      self.add_cookies("pw", pword)
+      user=str(user)
+      us=self.start_secret(user)
+      self.add_cookies("user", user)
+      self.add_cookies("us", us)
+      return pword
 
       #aqui le agregamos el objeto con la informacion
-    	#datos= db.GqlQuery("SELECT * FROM Db " "ORDER BY fecha DESC")
-    	#reg={}
-       #	for data in datos:
-    		#reg[data.reguser]=[data.regpass]
-    	#self.write(reg)
-    	#user=self.request.get("user")
-    	#password=self.request.get("pass")
-    	#if user and password:
-    	#	for r in reg:
-    	#		if user==r:
-    	#			if password==reg[r][0]:
-    	#				self.render("content.html", login=1)
-    	#else: 				
-    	#	self.render("content.html", login=0)
+      #datos= db.GqlQuery("SELECT * FROM Db " "ORDER BY fecha DESC")
+      #reg={}
+       #  for data in datos:
+        #reg[data.reguser]=[data.regpass]
+      #self.write(reg)
+      #user=self.request.get("user")
+      #password=self.request.get("pass")
+      #if user and password:
+      # for r in reg:
+      #   if user==r:
+      #     if password==reg[r][0]:
+      #       self.render("content.html", login=1)
+      #else:        
+      # self.render("content.html", login=0)
 class Reg(Handler):
   def get(self):
     user = self.request.get("username")
